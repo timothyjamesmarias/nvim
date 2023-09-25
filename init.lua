004d25a41becc18d4090ba9955aaec4799a20c47
Timothy Marias <timothy.marias@bythepixel.com>
2023-09-25 09:14:48 -0700

    updated section separators on lualine

--- init.lua
+++ init.lua
@@ -230,8 +230,8 @@ require("lazy").setup({
         options = {
           icons_enabled = true,
           theme = 'auto',
-          component_separators = { left = '', right = '' },
-          section_separators = { left = '', right = '' },
+          component_separators = { left = '|', right = '|' },
+          section_separators = { left = '', right = '' },
           disabled_filetypes = {
             statusline = {},
             winbar = {},
