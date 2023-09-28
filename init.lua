d3fd7b5badcd6347ef6c937f115b3f27931ecf0b
timothyjamesmarias <timothyjamesmarias@gmail.com>
2023-09-27 20:39:16 -0700

    keymaps for trouble

--- init.lua
+++ init.lua
@@ -492,13 +492,30 @@ require("lazy").setup({
 	{
 		"folke/twilight.nvim",
 	},
-  {
-    "folke/trouble.nvim",
-    config = function()
-      require("trouble").setup({})
-      vim.keymap.set("n", "<leader>D", ":TroubleToggle<CR>")
-    end,
-  }
+	{
+		"folke/trouble.nvim",
+		config = function()
+			require("trouble").setup({})
+			vim.keymap.set("n", "<leader>xx", function()
+				require("trouble").toggle()
+			end)
+			vim.keymap.set("n", "<leader>xw", function()
+				require("trouble").open("workspace_diagnostics")
+			end)
+			vim.keymap.set("n", "<leader>xd", function()
+				require("trouble").open("document_diagnostics")
+			end)
+			vim.keymap.set("n", "<leader>xq", function()
+				require("trouble").open("quickfix")
+			end)
+			vim.keymap.set("n", "<leader>xl", function()
+				require("trouble").open("loclist")
+			end)
+			vim.keymap.set("n", "gR", function()
+				require("trouble").open("lsp_references")
+			end)
+		end,
+	},
 })
 
 -- options
